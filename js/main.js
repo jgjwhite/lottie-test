cd1196ec48f97bf8b6f35bdf99568290aee98dd3
Justin White <justin.gj.white@gmail.com>
2018-03-14 22:28:42 -0400

    basic animation ready

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-$(document).foundation()
+
+  var animation = bodymovin.loadAnimation({
+    container: document.getElementById('bm'), // Required
+    path: 'data.json', // Required
+    renderer: 'svg/canvas/html', // Required
+    loop: true, // Optional
+    autoplay: true, // Optional
+    name: "Hello World", // Name for future reference. Optional.
+  })
